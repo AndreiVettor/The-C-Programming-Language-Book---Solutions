b04dd3b45fc101ebe9fe1028b403b14ebd88f177
Stefan-Andrei Vettor <stefan-andrei.vettor@continental-corporation.com>
2019-02-05 14:42:53 +0200

    Solves exercise 11, chapter 5 in a new way, adds vscode config files

--- Chapter5/chapter5_11.c
+++ Chapter5/chapter5_11.c
@@ -1,125 +1,99 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-#define TAB_LENGTH 8
+#define MAXLEN 1000
+#define MAXTABS 100
+#define DEFAULTTABSIZE 8
 
-void entab(char str[], unsigned int size, int tabs[]);
-void detab(char line[], int size, int tabs[]);
-void insertSpaces(char string[], int index, int amount, int size);
-int getNextTabPosition(int index, int tablen);
-int count_spaces(char str[], int tabs_index, int tabs[]);
-
-int main(int argc, char **argv)
+void detab(char* input, char* output, int tabc, int tablen[])
 {
-    char s[] = "Hello\tthere kind sir\thow are you doing?";
-    printf("input: %s\n", s);
-    int tabs[] = {8, 8, 8, 8, 8, 8};
+    int i = 0, j = 0;
+    int tabcount = 1;
 
-    detab(s, strlen(s), tabs);
+    // Define first tabstop
+    int tabstop = tablen[tabcount];
 
-    printf("detab: %s\n", s);
-
-    return 0;
-}
-
-#define MAX_LENGTH 1000
-
-void entab(char str[], unsigned int size, int tabs[])
-{
-    char result[MAX_LENGTH];
-    unsigned int i, j;
-    unsigned int result_size = 0;
-    unsigned int num_copy, spaces;
-
-    for(i = 0; i < size; i += tabs[i])
+    while(input[i] != '\n')
     {
-        // Check how many characters to copy
-        spaces = count_spaces(str, i, tabs);
-        num_copy = tabs[i] - spaces;
-
-        // If at the end of the string update num_copy
-        if(i + num_copy > size) {
-            num_copy = size - i - spaces;
+        // Track current tabstop
+        if(j >= tabstop)
+        {
+            if(tabcount >= tabc)
+            {
+                tabstop += DEFAULTTABSIZE;
+            }
+            else
+            {
+                tabstop += tablen[tabcount++];
+            }
         }
 
-        // Copy the characters (forget the spaces)
-        for(j = result_size; j < result_size + num_copy; ++j)
+        // Check if there is a tab character
+        //  if there's a tab character fill the rest of characters with spaces
+        if(input[i] == '\t')
         {
-            result[j] = str[i + j - result_size];
-        }
-
-        // If there were any spaces replace them with a TAB
-        if(spaces)
-        {
-            printf("I: %d\n", i);
-            result[j] = '@';
-            ++result_size;
-        }
-
-        // Update the result string size
-        result_size += num_copy;
-    }
-
-    result[result_size] = '\0';
-
-    strcpy(str, result);
-}
-
-int count_spaces(char str[], int tabs_index, int tabs[])
-{
-    int i;
-    int spaces = 0;
-
-    for(i = tabs_index + tabs[i] - 1; i >= tabs_index; --i)
-    {
-        if(str[i] == ' ')
-        {
-            ++spaces;
+            int spaces;
+            for(spaces = tabstop - j; spaces > 0; --spaces, ++j)
+            {
+                output[j] = ' ';
+            }
+            --j;
         }
         else
         {
-            break;
+            output[j] = input[i];
         }
-    }
 
-    return spaces;
-}
-
-void detab(char line[], int size, int tabs[])
-{
-    int i = 0;
-    while(line[i] != '\0') {
-        if(line[i] == '\t') {
-            insertSpaces(line, i, getNextTabPosition(i, tabs[i]), size);
-        }
         ++i;
+        ++j;
     }
+
+    output[j++] = '\n';
+    output[j] = '\0';
 }
 
-void insertSpaces(char string[], int index, int amount, int size) 
+int getline(char* s, int maxlen)
+{
+    int c, i = 0;
+
+    while((c = getchar()) != EOF && c != '\n') 
+    {
+        s[i++] = c;
+    }
+    if(c == '\n')
+    {
+        s[i++] = c;
+    }
+    s[i] = '\0';
+
+    return i;
+}
+
+void convargs(int argc, char* argv[], int* tablens)
 {
     int i;
-
-    // Shift all the characters to the left by amount units
-    for(i = size + 2; i > index; --i) {
-        string[i + amount - 1] = string[i];
-    }
-
-    // Put space characters in the new locations
-    for(i = index; i < index + amount; ++i) {
-        string[i] = ' ';
-    }
-}
-
-int getNextTabPosition(int index, int tablen)
-{
-    return tablen - (index % tablen);
-}
-
-int getTabLength(int index, int tabs[])
-{
-    while(index)
+    
+    for(i = 1; i < argc && i < MAXTABS; ++i) 
     {
-        
+        tablens[i] = atoi(argv[i]);
     }
+}
+
+int main(int argc, char* argv[])
+{
+    char input[MAXLEN];
+
+    getline(input, MAXLEN);
+
+    int tablens[MAXTABS];
+    convargs(argc, argv, tablens);
+    
+    // Make a new string that has enough space to accomodate new characters
+    char output[MAXLEN];
+    detab(input, output, argc, tablens);
+
+    printf("%s", output);
+
+    return 0;
 }
