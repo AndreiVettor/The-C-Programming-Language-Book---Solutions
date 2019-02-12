76834bcd118e793cad20cec08ba715afb264c570
Vettor <uia93167@contiwan.com>
2019-02-12 11:23:18 +0200

    Implement full solution for exercise 5-11 and config file cleanup

--- Chapter5/chapter5_11.c
+++ Chapter5/chapter5_11.c
@@ -6,15 +6,17 @@
 #define MAXTABS 100
 #define DEFAULTTABSIZE 8
 
-void detab(char* input, char* output, int tabc, int tablen[])
+void detab(char input[], int tabc, int tablen[])
 {
-    int i = 0, j = 0;
+    char output[MAXLEN];
+
+    int i, j;
     int tabcount = 1;
 
     // Define first tabstop
     int tabstop = tablen[tabcount];
 
-    while(input[i] != '\n')
+    for(i = 0, j = 0; input[i] != '\0'; ++i, ++j)
     {
         // Track current tabstop
         if(j >= tabstop)
@@ -29,7 +31,6 @@ void detab(char* input, char* output, int tabc, int tablen[])
             }
         }
 
-        // Check if there is a tab character
         //  if there's a tab character fill the rest of characters with spaces
         if(input[i] == '\t')
         {
@@ -44,16 +45,58 @@ void detab(char* input, char* output, int tabc, int tablen[])
         {
             output[j] = input[i];
         }
-
-        ++i;
-        ++j;
     }
 
-    output[j++] = '\n';
     output[j] = '\0';
+
+    strcpy(input, output);
 }
 
-int getline(char* s, int maxlen)
+void entab(char input[], int tabc, int tablen[])
+{
+    int i, j = 0;
+    int tabcount = 1;
+    int spaces = 0;
+
+    char result[MAXLEN];
+
+    for(i = 0; input[i] != '\0'; ++i, ++j)
+    {
+        if(i == tablen[tabcount])
+        {
+            if(tabcount >= tabc)
+            {
+                tablen[tabcount] = DEFAULTTABSIZE;
+            }
+            else
+            {
+                ++tabcount;
+            }
+
+            if(spaces > 0)
+            {
+                result[j++] = '\t';
+                spaces = 0;
+            }
+        }
+
+        if(input[i] == ' ')
+        {
+            ++spaces;
+        }
+        else
+        {
+            result[j] = input[i];
+            spaces = 0;
+        }
+    }
+
+    result[++j] = '\0';
+
+    strcpy(result, input);
+}
+
+int getline(char s[], int maxlen)
 {
     int c, i = 0;
 
@@ -70,10 +113,13 @@ int getline(char* s, int maxlen)
     return i;
 }
 
-void convargs(int argc, char* argv[], int* tablens)
+void convargs(int argc, char* argv[], int tablens[])
 {
     int i;
     
+    // Convert unused position to a 0-index reference
+    tablens[0] = 0;
+
     for(i = 1; i < argc && i < MAXTABS; ++i) 
     {
         tablens[i] = atoi(argv[i]);
@@ -82,18 +128,18 @@ void convargs(int argc, char* argv[], int* tablens)
 
 int main(int argc, char* argv[])
 {
-    char input[MAXLEN];
+    char text[MAXLEN];
 
-    getline(input, MAXLEN);
+    getline(text, MAXLEN);
 
     int tablens[MAXTABS];
     convargs(argc, argv, tablens);
-    
-    // Make a new string that has enough space to accomodate new characters
-    char output[MAXLEN];
-    detab(input, output, argc, tablens);
 
-    printf("%s", output);
+    detab(text, argc, tablens);
+    printf("%s", text);
+
+    entab(text, argc, tablens);
+    printf("%s", text);
 
     return 0;
 }
