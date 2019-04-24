9da9015b74fe96632e5c87c9369ac044a805e600
Stefan-Andrei Vettor <stefan-andrei.vettor@continental-corporation.com>
2019-04-24 08:51:39 +0300

    Started working on 5-14

--- Chapter5/chapter5_14/qsort.c
+++ Chapter5/chapter5_14/qsort.c
@@ -12,33 +12,143 @@
 
 #define MAXLEN 1000
 #define MAXLINES 5000
+#define ALLOCSIZE 10000
 
+static char allocbuf[ALLOCSIZE];
+static char *allocp = allocbuf;
 char *lineptr[MAXLINES];
-int numeric = 0;
-int reverse = 0;
+int numeric;
+int reverse;
 
-int readlines(char *lineptr[], int nlines);
+int readlines(char *v[], int nlines);
 void writelines(char *lineptr[], int nlines);
 void qsort(void *lineptr[], int left, int right,
     int (*comp)(void *, void *));
+void qsort_r(void *v[], int left, int right,
+    int (*comp)(void*, void*));
+void swap(void *v[], int i, int j);
 int numcmp(char *, char *);
 void convargs(int argc, char* argv[]);
+char *alloc(int n);
 
 int main(int argc, char* argv[])
 {
     int nlines;
 
     convargs(argc, argv);
+    if((nlines = readlines(lineptr, MAXLINES)) >= 0)
+    {
+        if(reverse)
+        {
+            qsort_r((void**) lineptr, 0, nlines - 1,
+                numeric ? (int (*)(void*, void*))numcmp 
+                : (int (*)(void*, void*))strcmp);
+        }
+        else
+        {
+            qsort((void**) lineptr, 0, nlines - 1,
+                numeric ? (int (*)(void*, void*))numcmp 
+                : (int (*)(void*, void*))strcmp);
+        }
+        
+        writelines(lineptr, nlines);
+        return 0;
+    }
+    else
+    {
+        printf("Input too big to sort\n");
+        return 1;
+    }
     
-    readlines(lineptr, MAXLINES);
-
 
     return 0;
+}
+
+void qsort(void *v[], int left, int right,
+    int (*comp)(void*, void*))
+{
+    int i, pivot;
+
+    if(left >= right)
+    {
+        return;
+    }
+
+    swap(v, left, (left + right) / 2);
+
+    pivot = left;
+    for(i = left + 1; i <= right; ++i)
+    {
+        if((*comp)(v[i], v[left]) < 0)
+        {
+            swap(v, ++left, i);
+        }
+    }
+    swap(v, left, pivot);
+    qsort(v, left, pivot - 1, comp);
+    qsort(v, pivot + 1, right, comp);
+}
+
+void qsort_r(void *v[], int left, int right,
+    int (*comp)(void*, void*))
+{
+    int i, pivot;
+
+    if(left >= right)
+    {
+        return;
+    }
+
+    swap(v, left, (left + right) / 2);
+
+    pivot = left;
+    for(i = left + 1; i <= right; ++i)
+    {
+        if((*comp)(v[i], v[left]) > 0)
+        {
+            swap(v, ++left, i);
+        }
+    }
+    swap(v, left, pivot);
+    qsort(v, left, pivot - 1, comp);
+    qsort(v, pivot + 1, right, comp);
+}
+
+void swap(void *v[], int i, int j)
+{
+    void *temp;
+
+    temp = v[i];
+    v[i] = v[j];
+    v[j] = temp;
+}
+
+int numcmp(char *s1, char *s2)
+{
+    double v1, v2;
+
+    v1 = atof(s1);
+    v2 = atof(s2);
+    if(v1 < v2)
+    {
+        return -1;
+    }
+    else if(v1 > v2)
+    {
+        return 1;
+    }
+    else 
+    {
+        return 0;
+    }
 }
 
 void convargs(int argc, char* argv[]) 
 {
     int i;
+
+    numeric = 0;
+    reverse = 0;
 
     if(argc > 1)
     {
@@ -63,7 +173,7 @@ void convargs(int argc, char* argv[])
     }
 }
 
-int readlines(char *lineptr[], int nlines)
+int readlines(char *lineptr[], int maxlines)
 {
     int len, nlines;
     char *p, line[MAXLEN];
@@ -71,7 +181,7 @@ int readlines(char *lineptr[], int nlines)
     nlines = 0;
     while((len = getln(line, MAXLEN)) > 0)
     {
-        if(nlines > MAXLINES || (p = alloc(len) == NULL))
+        if(nlines > maxlines || (p = alloc(len)) == NULL)
         {
             return -1;
         }
@@ -84,6 +194,14 @@ int readlines(char *lineptr[], int nlines)
     }
 
     return nlines;
+}
+
+void writelines(char *lineptr[], int nlines)
+{
+    while (nlines--)
+    {
+        printf("%s\n", *lineptr++);
+    }
 }
 
 int getln(char s[], int maxlen)
@@ -103,4 +221,17 @@ int getln(char s[], int maxlen)
     s[i] = '\0';
 
     return i;
+}
+
+char *alloc(int n)
+{
+    if (allocbuf + ALLOCSIZE - allocp >= n)
+    {
+        allocp += n;
+        return allocp - n;
+    }
+    else
+    {
+        return 0;
+    }
 }
